7f3b7ebae355c0af38e4590d7fd93ea630505a2b
Elena-Tsimafeyeva <elenatsimafeyeva@gmail.com>
2024-08-14 21:09:54 +0300

    feat: Added "ChekTheEnteredWordAgainstTheMainWord" method to check the entered word against the main word.

--- WordGame/Program.cs
+++ WordGame/Program.cs
@@ -1,8 +1,6 @@
 ﻿string symbols = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyz1234567890!?@#$%^&*()_-=+№;:<>.,/|`~{}[] ";
 string? word, word1, word2;
 int num = 0;
-int letterCounterForMainWord;
-int letterCounterForUserWord;
 EnterTheMainWord(out word);//Ввод первоначального слова
 CheckTheMainWord(word);//Проверка первоначального слова
 do
@@ -39,8 +37,6 @@ void CheckTheMainWord(string? word)
 }
 void Game(string symbols, string? word, string? word1, int turn)//Проверка слова введёного игроком по отношению к первоначальному слову
 {
-    letterCounterForMainWord = 0;
-    letterCounterForUserWord = 0;
     if (word1.Length == 0)//Проверка ввёл ли игрок слово
     {
         Console.WriteLine("Вы ничего не ввели :(");
@@ -49,34 +45,39 @@ void Game(string symbols, string? word, string? word1, int turn)//Проверк
     }
     else //Если игрок ввёл слово
     {
-        for (int i = 0; i < symbols.Length; i++)//Проверка введённого слова по отношению первоначальному слову
+        ChekTheEnteredWordAgainstTheMainWord(symbols, word, word1, turn);
+    }
+}
+void ChekTheEnteredWordAgainstTheMainWord(string symbols, string? word, string? word1, int turn)
+{
+    int letterCounterForMainWord = 0;
+    int letterCounterForUserWord = 0;
+    for (int i = 0; i < symbols.Length; i++)//Проверка введённого слова по отношению первоначальному слову
+    {
+        if (i != 0)
         {
-            if (i != 0)
+            if (letterCounterForMainWord < letterCounterForUserWord)
             {
-                if (letterCounterForMainWord < letterCounterForUserWord)
-                {
-                    Console.WriteLine($"Игра окончена! Победил игрок {turn}!");
-                    Environment.Exit(1);
-                }
-            }
-            letterCounterForMainWord = 0;
-            letterCounterForUserWord = 0;
-            for (int j = 0; j < word.Length; j++)
-            {
-                if (symbols[i] == word[j])
-                {
-                    letterCounterForMainWord++;
-                }
-            }
-            for (int j = 0; j < word1.Length; j++)
-            {
-                if (symbols[i] == word1[j])
-                {
-                    letterCounterForUserWord++;
-                }
+                Console.WriteLine($"Игра окончена! Победил игрок {turn}!");
+                Environment.Exit(1);
             }
         }
-
+        letterCounterForMainWord = 0;
+        letterCounterForUserWord = 0;
+        for (int j = 0; j < word.Length; j++)
+        {
+            if (symbols[i] == word[j])
+            {
+                letterCounterForMainWord++;
+            }
+        }
+        for (int j = 0; j < word1.Length; j++)
+        {
+            if (symbols[i] == word1[j])
+            {
+                letterCounterForUserWord++;
+            }
+        }
     }
 }
 void FirstPlayerEnterTheWord(out string? word1)
